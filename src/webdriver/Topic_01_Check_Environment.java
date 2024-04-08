0dbc13e68568307beade0b36a61f506367c0a90f
trangntt <nguyentthuytrang1110@gmail.com>
2024-04-08 21:33:10 +0700

    Update  Topic 01

--- src/webdriver/Topic_01_Check_Environment.java
+++ src/webdriver/Topic_01_Check_Environment.java
@@ -46,6 +46,11 @@ public class Topic_01_Check_Environment {
         Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
     }
 
+    @Test
+    public void TC_04() {
+        //...
+    }
+
     @AfterClass
     public void afterClass() {
         driver.quit();
